c7c460358cc7064f3a066a3c60fb45227b0130e5
Lucif3r1101 <rishav1101@gmail.com>
2021-08-14 02:16:42 +0530

    Fixing

--- src/Chats/Chats.js
+++ src/Chats/Chats.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router , Switch, Route, Link}  from "react-router-dom";
+
 import "./Chats.css";
 import Chat from "./Chat"
 
@@ -7,10 +7,18 @@ function Chats() {
     return (
         <div className="chats">
             <Chat 
-              name="Selina Kyle"
+              name = "Selina Kyle"
               message = "How are you?"
               timestamp = "35 minutes ago"
-              profilePic="https://th.bing.com/th/id/OIP.OhtRVOGsVizXwlwE8cdX6wHaEO?pid=ImgDet&rs=1"
+              profilePic ="https://th.bing.com/th/id/OIP.OhtRVOGsVizXwlwE8cdX6wHaEO?pid=ImgDet&rs=1"
+              
+            />
+            
+            <Chat 
+              name = "Pepper Potts"
+              message = "Where are you?"
+              timestamp = "30 minutes ago"
+              profilePic = "https://image.pngaaa.com/477/3616477-middle.png"
             />
 
             
